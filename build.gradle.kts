f93ba14b88f6ded3983c6e752b5ebad537e9c094
Devonshin <44950849+Devonshin@users.noreply.github.com>
2025-02-15 22:22:10 +0100

    Github Action 적용
    Github Action 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,29 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
     id("idea")
     kotlin("plugin.power-assert") version "2.0.0"
+    `maven-publish`
 }
 
-group = "com.gatchii.common-util"
+group = "com.gatchii"
 version = "0.0.1"
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Devonshin/gatchii-common-util")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
@@ -28,29 +47,14 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-host-common-jvm")
-    implementation("io.ktor:ktor-server-status-pages-jvm")
-    implementation("io.ktor:ktor-server-caching-headers-jvm")
-    implementation("io.ktor:ktor-server-compression-jvm")
-    implementation("com.ucasoft.ktor:ktor-simple-cache-jvm:0.4.3")
-    implementation("com.ucasoft.ktor:ktor-simple-redis-cache-jvm:0.4.3")
-    implementation("io.ktor:ktor-server-call-logging-jvm")
-    implementation("io.ktor:ktor-server-call-id-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    //implementation("io.ktor:ktor-server-routing-jvm:$ktorVersion")
 
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
-    implementation("io.ktor:ktor-server-config-yaml")
     // https://mvnrepository.com/artifact/org.bouncycastle/bcprov-jdk18on
     implementation("org.bouncycastle:bcprov-jdk18on:1.79")
     // https://mvnrepository.com/artifact/org.bitbucket.b_c/jose4j
     implementation("org.bitbucket.b_c:jose4j:0.9.6")
     implementation("com.nimbusds:nimbus-jose-jwt:9.45")
     implementation("org.mindrot:jbcrypt:$jbcryptVersion")
-    testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:$kotlinVersion")
     testImplementation("io.mockk:mockk:${mockkVersion}")
     testImplementation("org.assertj:assertj-core:3.11.1")
